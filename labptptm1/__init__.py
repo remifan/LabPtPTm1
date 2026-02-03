bf2fc60824efae8ecdd3eabd11436365acc02d24
Remi QR FAN <remi.qr.fan@gmail.com>
2026-02-04 00:01:10 +0800

    Upgrade to zarr 3.x compatibility
    - Update zarr dependency from 2.9.5 to >=3.0
    - Update zarr.open_group call with use_consolidated=True and zarr_format=2
    - Add 'rich' dependency for .tree() method support
    - Bump version to 0.2.0
    
    Thanks Claude for the help with this migration! 🎉

--- labptptm1/__init__.py
+++ labptptm1/__init__.py
@@ -1,4 +1,11 @@
+import warnings
 import zarr
 
-dataset = zarr.open_group("simplecache::s3://optcommpubdataqrfan/labptptm1_zarr",
-                          storage_options={"s3": {'anon': True}})
+# Suppress async warning from zarr since simplecache doesn't support async mode
+with warnings.catch_warnings():
+    warnings.filterwarnings('ignore', message='.*asynchronous.*', category=UserWarning)
+    dataset = zarr.open_group("simplecache::s3://optcommpubdataqrfan/labptptm1_zarr",
+                              mode='r',
+                              storage_options={"s3": {'anon': True}},
+                              use_consolidated=True,
+                              zarr_format=2)
--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@
 from setuptools import setup, find_packages
 
 setup(name='labptptm1',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
-        'zarr==2.9.5',
+        'zarr>=3.0',
         'fsspec',
-        's3fs'
+        's3fs',
+        'rich'
     ]
 )
